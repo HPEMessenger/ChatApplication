bd81aefd35b410735969629de6739c3f683322bb
HPEMessenger <fionajovita97@yahoo.in>
2018-02-12 18:01:45 +0530

    new commit

--- app/src/main/java/com/example/fiona/chatapplication/ProfileActivity.java
+++ app/src/main/java/com/example/fiona/chatapplication/ProfileActivity.java
@@ -1,12 +1,17 @@
 package com.example.fiona.chatapplication;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,18 +23,25 @@ public class ProfileActivity extends AppCompatActivity {
     private Button send_request,decline_request;
     private TextView profile_username,profile_status;
     private ImageView profileImage;
-    private DatabaseReference UsersReference;
+    private DatabaseReference UsersReference,FriendRequestReference;
+    private String current_state;
+    private FirebaseAuth mAuth;
+    String sender_user_id,receiver_user_id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        String visit_user_id=getIntent().getExtras().get("User_visit_id").toString();
+        receiver_user_id=getIntent().getExtras().get("User_visit_id").toString();
+        UsersReference= FirebaseDatabase.getInstance().getReference().child("Users").child(receiver_user_id);
+        FriendRequestReference=FirebaseDatabase.getInstance().getReference().child("Friend_Request");
+        mAuth=FirebaseAuth.getInstance();
+        sender_user_id=mAuth.getCurrentUser().getUid();
         send_request=findViewById(R.id.profile_visit_send_request_btn);
         decline_request=findViewById(R.id.profile_visit_decline_request_btn);
         profile_username=findViewById(R.id.profile_visit_username);
         profile_status=findViewById(R.id.profile_visit_status);
         profileImage=findViewById(R.id.profile_visit_user_image);
-        UsersReference= FirebaseDatabase.getInstance().getReference().child("Users").child(visit_user_id);
+        current_state="not_friends";
         UsersReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -46,5 +58,31 @@ public class ProfileActivity extends AppCompatActivity {
 
             }
         });
+        send_request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send_request.setEnabled(false);
+                if(current_state.equals("not_friends")){
+                    SendFriendRequestToAPerson();
+                }
+            }
+        });
     }
+
+    private void SendFriendRequestToAPerson() {
+        FriendRequestReference.child(sender_user_id)
+                .child(receiver_user_id)
+                .child("request_type")
+                .setValue("sent")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+
+                        }
+                    }
+                });
+    }
+
+
 }
