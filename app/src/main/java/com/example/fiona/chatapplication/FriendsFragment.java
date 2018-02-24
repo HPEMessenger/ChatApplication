66dfd8e98d3c34c56b648af4b6447302baefb81c
HPEMessenger <fionajovita97@yahoo.in>
2018-02-24 21:06:51 +0530

    new commit

--- app/src/main/java/com/example/fiona/chatapplication/FriendsFragment.java
+++ app/src/main/java/com/example/fiona/chatapplication/FriendsFragment.java
@@ -1,6 +1,7 @@
 package com.example.fiona.chatapplication;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,8 +13,16 @@ import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.NetworkPolicy;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 /**
@@ -22,7 +31,7 @@ import com.google.firebase.database.FirebaseDatabase;
 public class FriendsFragment extends Fragment {
 
     private RecyclerView myFriendList;
-    private DatabaseReference friendsReference;
+    private DatabaseReference friendsReference,usersReference;
     private FirebaseAuth mAuth;
     String online_user_id;
     private View myMainView;
@@ -38,9 +47,12 @@ public class FriendsFragment extends Fragment {
         myFriendList = (RecyclerView)myMainView.findViewById(R.id.friends_list);
         mAuth = FirebaseAuth.getInstance();
         online_user_id = mAuth.getCurrentUser().getUid();
+        usersReference = FirebaseDatabase.getInstance().getReference().child("Users");
+        usersReference.keepSynced(true);
         friendsReference = FirebaseDatabase.getInstance().getReference()
                 .child("Friends")
                 .child(online_user_id);
+        friendsReference.keepSynced(true);
         myFriendList.setLayoutManager(new LinearLayoutManager(getContext()));
         return myMainView;
     }
@@ -61,12 +73,27 @@ public class FriendsFragment extends Fragment {
                     protected void populateViewHolder(FriendsViewHolder viewHolder, Friends model, int position) {
                         viewHolder.setDate(model.getDate());
                         String list_user_id = getRef(position).getKey();
+                        usersReference.child(list_user_id)
+                                .addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        String User_name = dataSnapshot.child("User_name").getValue().toString();
+                                        String thumb_image = dataSnapshot.child("User_thumb_image").getValue().toString();
+                                        FriendsViewHolder.setUsername(User_name);
+                                        FriendsViewHolder.setThumbImage(thumb_image,getContext());
+                                    }
+
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
+
+                                    }
+                                });
                     }
                 };
         myFriendList.setAdapter(firebaseRecyclerAdapter);
     }
     public static class FriendsViewHolder extends RecyclerView.ViewHolder{
-        View mView;
+        static View mView;
         public FriendsViewHolder(View itemView) {
             super(itemView);
             mView = itemView;
@@ -74,6 +101,27 @@ public class FriendsFragment extends Fragment {
         public void setDate(String date) {
             TextView sinceFriendDate = (TextView)mView.findViewById(R.id.all_users_status);
             sinceFriendDate.setText(date);
+        }
+        public static void setUsername(String User_name){
+            TextView usernameDisplay =(TextView)mView.findViewById(R.id.all_users_username);
+            usernameDisplay.setText(User_name);
+        }
+
+        public static void setThumbImage(final String thumb_image,final Context ctx) {
+            final CircleImageView User_thumb_image=(CircleImageView)mView.findViewById(R.id.all_users_profile_image);
+
+            Picasso.with(ctx).load(thumb_image).networkPolicy(NetworkPolicy.OFFLINE).placeholder(R.drawable.default_image).into(User_thumb_image, new Callback() {
+                @Override
+                public void onSuccess() {
+
+                }
+
+                @Override
+                public void onError() {
+                    Picasso.with(ctx).load(thumb_image).placeholder(R.drawable.default_image).into(User_thumb_image);
+
+                }
+            });
         }
     }
 }
